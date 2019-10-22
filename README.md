49282992cef2ac4bd6209a15ccb3a69b200666a7
Brian Anstett <brian.anstett@24g.com>
2019-10-21 20:36:59 -0400

    init

---
+++ README.md
@@ -0,0 +1,57 @@
+# @24g/vault
+Simple module for accessing Vault secrets. Includes support for Lambda and containers.
+
+```Javascript
+const vault = require('@24g/vault');
+
+vault('secret/data/foo')
+  .then(console.log)
+  .catch(console.log)
+```
+---
+
+## Options
+Method can take an optional options object for advanaced configuration.
+* `vaultAddress`: Vault endpoint to use. Defaults to the `VAULT_ADDR` environment variable.
+* `vaultTokenPath`: Location of the Vault session token on the filesystem. Defaults to the `VAULT_TOKEN_PATH` environment variable or `~/.vault-token` if not set. This option is only needed when running on a server.
+* `vaultRole`: What [Vault role](https://www.vaultproject.io/api/auth/aws/index.html#create-role) to attempt to authenticate to. Defaults `VAULT_ROLE` environments variable. This option is only needed when running on Lambda.
+* `awsScopedCredentialsRegion`: Which region the [STS signature is scoped to](https://docs.aws.amazon.com/general/latest/gr/sigv4_changes.html). Defaults to the `VAULT_AWS_SCOPED_CREDENTIALS_REGION` environment variable or `us-east-1` if not set. This option is only needed when running on Lambda.
+
+```Javascript
+const vault = require('@24g/vault');
+const options= {
+  vaultAddress:'https://vault.24g.dev',
+  vaultRole: 'g-1234-developer-role'
+};
+
+vault('secret/data/foo', options)
+  .then(console.log)
+  .catch(console.log)
+```
+---
+
+## Differences when running on a server vs Lambda
+When running on a server, it is assumed you already have a [Auth Agent](https://www.vaultproject.io/docs/agent/) for your [authorization method](https://www.vaultproject.io/docs/auth/index.html) configured and are logged in (already retrieved a session token). When running on Kubernetes, our [single_server_standard](https://bitbucket.org/24g/24g-architecture/src/master/Kubernetes/helm/scaffold/single_server_standard/) scaffold comes preconfigured with a auth agent sidecar to retrieve and renew a session token. In that circumstance, this modules acts as an API wrapper.
+
+The following options or environment variables must be set.
+* `options.vaultAddress` || `VAULT_ADDR`
+* `options.vaultTokenPath` || `VAULT_TOKEN_PATH`
+
+When running in Lambda, this module will [authenticate to Vault](https://www.vaultproject.io/docs/auth/aws.html) and then make the necessary API requests to retrieve the desired secret. The secret values are cached for the duration of the function invokation. 
+
+The following topions or environment variables must be set.
+* `options.vaultAddress` || `VAULT_ADDR`
+* `options.vaultRole` || `VAULT_ROLE`
+* `options.ScopedCredentialsRegion` || `VAULT_AWS_SCOPED_CREDENTIALS_REGION`
+
+## Debug
+This module uses [debug](https://www.npmjs.com/package/debug) to log advanced information. Set the environment variable `DEBUG` to `vault:*`.
+
+```Bash
+DEBUG=vault:* node index.js
+vault:vaultAwsAuth:authenticate { errors: [ 'entry for role g-1234-developer-role not found' ] } +0ms
+Error: Error while trying to authenticate to vault server.
+    at getSecret (/home/briananstett/work/@24g-vault/index.js:86:13)
+    at process._tickCallback (internal/process/next_tick.js:68:7)
+
+```
---
+++ index.js
@@ -0,0 +1,114 @@
+/**
+ * @author Brian Anstett
+ */
+
+const Vault = require('node-vault');
+const vaultAwsAuth = require('./libs/vault-auth-aws');
+const fs = require('fs');
+const debug = require('debug');
+const os = require('os');
+
+// For caching
+const secretValues = {};
+
+/**
+ * fs promise is still experimental. fs_readFile is a promise wrapper around fs.readFile.
+ * @param  {...any} args 
+ */
+async function fs_readFile(...args){
+  return new Promise((resolve, reject)=>{
+    fs.readFile(...args, (error, data)=>{
+      if(error) return reject(error);
+      resolve(data);
+    })
+  })
+}
+
+/**
+ * https://docs.aws.amazon.com/IAM/latest/UserGuide/id_credentials_temp_enable-regions.html#id_credentials_region-endpoints
+ * @param {String} region AWS region
+ * @returns {String} AWS STS region endpoint
+ */
+function getAwsRequestUrl(region){
+  const awsRegion = region || process.env.AWS_REGION || process.env.AWS_DEFAULT_REGION ;
+
+  if(!awsRegion) throw new Error(`Valid region is required to determine AWS Request URL. Got ${awsRegion}.`);
+  return 'https://sts.' + awsRegion + '.amazonaws.com';
+}
+
+/**
+ * @param {string} [secret] Vault secret path
+ * @param {Object} [options] Options object
+ * @param {String} [options.vaultAddress] Vault endpoint to use. Defaults to VAULT_ADDR environment variable.
+ * @param {String} [options.vaultTokenPath] Location of Vault session token on the file system. Defaults to VAULT_TOKEN_PATH environment variable
+ * @param {String} [options.vaultRole] Required if running in Lambda. What Vault role attempt to auth to.
+ * @param {String} [options.scopedCredentialsRegion] Which region the STS signature is scoped to.
+ */
+module.exports = async function getSecret(secretPath, options = {}) {
+  // To be backwards compatible, we still use the KEY_NAME env var.
+  // When secret manager is fully deprecated, we should swap this to SECRET_PATH
+  if (!secretPath) secretPath = process.env.SECRET_PATH || process.env.KEY_NAME;
+  // Check if secret exists in cache
+  if (secretValues[secretPath]) return secretValues[secretPath];
+
+  // Default to environment variables
+  const vaultAddress = options.vaultAddress || process.env.VAULT_ADDR; // Address for the Vault API.
+  const vaultTokenPath = options.vaultTokenPath || process.env.VAULT_TOKEN_PATH || os.homedir() + '/.vault-token'; // Required only for containers. Where on the FS the vault token lives.
+  const vaultRole = options.vaultRole || process.env.VAULT_ROLE; // Required only for Lambda. What Vault role to auth to.
+  const vaultPort = 443;  // Port of the Vault API.
+  const scopedCredentialsRegion = options.ScopedCredentialsRegion || process.env.VAULT_AWS_SCOPED_CREDENTIALS_REGION || 'us-east-1';
+  let token,vaultClient;
+
+  // Determine is being called in Lambda or container.
+  if(process.env.AWS_EXECUTION_ENV){
+    // Running in Lambda. Make necessary assumptions.
+    if(!vaultRole ) return new Error("Requires either options.vaultRole or VAULT_ROLE environment variable.");
+    if(!scopedCredentialsRegion ) return new Error("Requires either options.scopedCredentialsRegion or VAULT_AWS_SCOPED_CREDENTIALS_REGION environment variable.");
+    try{
+      vaultClient = new vaultAwsAuth({
+        host: vaultAddress,
+        vaultAppName: vaultRole, 
+        port: vaultPort,
+        region: scopedCredentialsRegion,
+        awsRequestUrl: getAwsRequestUrl()
+      });
+    }catch(vaultConstructorError){
+      debug('vault:vaultAwsAuth:constructor')(vaultConstructorError);
+      throw vaultConstructorError;
+    }
+
+    try{
+      const awsAuthResponse = await vaultClient.authenticate();
+      token = awsAuthResponse.auth.client_token;
+    }catch(awsAuthError){
+      debug('vault:vaultAwsAuth:authenticate')(awsAuthError);
+      throw new Error("Error while trying to authenticate to vault server.");
+    }
+  }else{
+    // Assume running in container
+    // Attempt to read Vault session token. Throw error otherwise.
+    try{
+      token = await fs_readFile(vaultTokenPath, { encoding: 'ascii' });
+    }catch(fsError){
+      throw fsError.message;
+    }
+  }
+
+  // Create Vault client
+  const vaultOptions = {
+    apiVersion: 'v1',
+    endpoint: vaultAddress,
+    token,
+  };
+  const vault = Vault(vaultOptions);
+
+  // Attempt to read secret from Valut. Throw error otherwise.
+  try{
+    const secret = await vault.read(secretPath);
+    secretValues[secretPath] = secret.data.data;
+    return secretValues[secretPath];
+  }catch(vaultError){
+    debug('vault:apiRequest')(vaultError);
+    throw vaultError
+  }
+};
