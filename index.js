551bbf356befc92c3b32f2e3dd86792fa8b8343c
Brian Anstett <brian.anstett@24g.com>
2020-11-06 10:05:39 -0500

    Support use of VAULT_TOKEN env var

--- index.js
+++ index.js
@@ -44,6 +44,7 @@ function getAwsRequestUrl(region) {
  * @param {Boolean} [options.bypassCache] Make a new API call for secrets instead of using cache.
  * @param {String} [options.vaultRole] Required if running in Lambda. What Vault role attempt to auth to.
  * @param {String} [options.scopedCredentialsRegion] Which region the STS signature is scoped to.
+ * @param {string} [options.vaultToken] An existing Vault auth token to use.
  */
 module.exports = async function getSecret(secretPath, options = {}) {
   // To be backwards compatible, we still use the KEY_NAME env var.
@@ -62,41 +63,45 @@ module.exports = async function getSecret(secretPath, options = {}) {
   const vaultTokenPath = options.vaultTokenPath || process.env.VAULT_TOKEN_PATH || `${os.homedir()}/.vault-token`; // Required only for containers. Where on the FS the vault token lives.
   const vaultRole = options.vaultRole || process.env.VAULT_ROLE; // Required only for Lambda. What Vault role to auth to.
   const vaultPort = 443; // Port of the Vault API.
+  let token = options.vaultToken || process.env.VAULT_TOKEN; // Use can pass in existing Vault token via options or environment variable.
   const scopedCredentialsRegion = options.awsScopedCredentialsRegion || process.env.VAULT_AWS_SCOPED_CREDENTIALS_REGION || 'us-east-1';
-  let token; let vaultClient;
+  let vaultClient;
 
-  // Determine is being called in Lambda or container.
-  if (process.env.AWS_EXECUTION_ENV) {
-    // Running in Lambda. Make necessary assumptions.
-    if (!vaultRole) return new Error('Requires either options.vaultRole or VAULT_ROLE environment variable.');
-    if (!scopedCredentialsRegion) return new Error('Requires either options.scopedCredentialsRegion or VAULT_AWS_SCOPED_CREDENTIALS_REGION environment variable.');
-    try {
-      vaultClient = new VaultAwsAuth({
-        host: vaultAddress,
-        vaultAppName: vaultRole,
-        port: vaultPort,
-        region: scopedCredentialsRegion,
-        awsRequestUrl: getAwsRequestUrl(),
-      });
-    } catch (vaultConstructorError) {
-      debug('vault:VaultAwsAuth:constructor')(vaultConstructorError);
-      throw vaultConstructorError;
-    }
+  // If the user passed in an existing Vault token, you that. Don't go through the process of generating one
+  if (token == null) {
+    // Determine if being called in Lambda or container.
+    if (process.env.AWS_EXECUTION_ENV) {
+      // Running in Lambda. Make necessary assumptions.
+      if (!vaultRole) return new Error('Requires either options.vaultRole or VAULT_ROLE environment variable.');
+      if (!scopedCredentialsRegion) return new Error('Requires either options.scopedCredentialsRegion or VAULT_AWS_SCOPED_CREDENTIALS_REGION environment variable.');
+      try {
+        vaultClient = new VaultAwsAuth({
+          host: vaultAddress,
+          vaultAppName: vaultRole,
+          port: vaultPort,
+          region: scopedCredentialsRegion,
+          awsRequestUrl: getAwsRequestUrl(),
+        });
+      } catch (vaultConstructorError) {
+        debug('vault:VaultAwsAuth:constructor')(vaultConstructorError);
+        throw vaultConstructorError;
+      }
 
-    try {
-      const awsAuthResponse = await vaultClient.authenticate();
-      token = awsAuthResponse.auth.client_token;
-    } catch (awsAuthError) {
-      debug('vault:VaultAwsAuth:authenticate')(awsAuthError);
-      throw new Error('Error while trying to authenticate to vault server.');
-    }
-  } else {
-    // Assume running in container
-    // Attempt to read Vault session token. Throw error otherwise.
-    try {
-      token = await fsReadFile(vaultTokenPath, { encoding: 'ascii' });
-    } catch (fsError) {
-      throw fsError.message;
+      try {
+        const awsAuthResponse = await vaultClient.authenticate();
+        token = awsAuthResponse.auth.client_token;
+      } catch (awsAuthError) {
+        debug('vault:VaultAwsAuth:authenticate')(awsAuthError);
+        throw new Error('Error while trying to authenticate to vault server.');
+      }
+    } else {
+      // Assume running in container
+      // Attempt to read Vault session token. Throw error otherwise.
+      try {
+        token = await fsReadFile(vaultTokenPath, { encoding: 'ascii' });
+      } catch (fsError) {
+        throw fsError.message;
+      }
     }
   }
 
@@ -114,6 +119,6 @@ module.exports = async function getSecret(secretPath, options = {}) {
     return secretValues[path];
   } catch (vaultError) {
     debug('vault:apiRequest')(vaultError);
-    throw vaultError;
+    throw vaultError.stack;
   }
 };
